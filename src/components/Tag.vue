bd288b77f7c6b437a57e01073dfe0cd461a8e0f1
王永森 <wangyongsen@istarshine.com>
2022-10-13 14:28:14 +0800

    基础应用

---
+++ src/components/Tag.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="tag">
+        <slot name="default"><span class="tag-default">我是一个标签</span></slot>
+    </div>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue'
+
+    export default defineComponent({
+        setup() {
+            return {}
+        }
+    })
+</script>
+
+<style scoped>
+    .tag {
+        display: inline-block;
+    }
+    .tag-default {
+        color: #f99;
+        font-size: 16px;
+    }
+</style>
